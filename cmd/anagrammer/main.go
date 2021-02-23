6a9bfbf4a3fa6db8386f05e794ac2414ab610b26
Matt Hoiland <matthew.proctor.hoiland@gmail.com>
2021-02-23 01:26:05 -0700

    Load wordlist into memory

--- cmd/anagrammer/main.go
+++ cmd/anagrammer/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	re "regexp"
+	s "strings"
+)
+
+const allCapsPattern = `^[A-Z]+$`
+
+func loadWordList() (words []string) {
+	fileName := "wordlist.txt"
+
+	fp, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalf("failed to open %s, err: %v", fileName, err)
+	}
+
+	scanner := bufio.NewScanner(fp)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		word := s.ToUpper(s.Split(s.TrimSpace(line), " ")[0])
+		if match, _ := re.MatchString(allCapsPattern, word); match {
+			words = append(words, word)
+		}
+	}
+	return
+}
+
+func validateWordList(words []string) bool {
+	for i, word := range words {
+		if match, _ := re.MatchString(allCapsPattern, word); !match {
+			fmt.Println(i, word)
+			return false
+		}
+	}
+	return true
+}
 
 func main() {
-	fmt.Println("The README lied to you.")
+	words := loadWordList()
+	if validateWordList(words) {
+		fmt.Println("Only ALLCAPS words here!")
+	} else {
+		fmt.Println("Something's fishy")
+	}
+	fmt.Printf("Total words: %d\n", len(words))
 }
